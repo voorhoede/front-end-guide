b774deb9b914f62920de0d3e1c84a87d7bf66250
Jasper Moelker <j.b.moelker@activeids.nl>
2014-10-11 17:50:33 +0200

    chore(setup): add basic assets structure combined into dist/assets/; add html+less watchers;

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,8 @@
 /* Dependencies (A-Z) */
+var browserSync = require('browser-sync');
 var del = require('del');
+var debug = require('gulp-debug');
+var gulpif = require('gulp-if');
 var filter = require('gulp-filter');
 var jshint = require('gulp-jshint');
 var fs = require('fs');
@@ -7,11 +10,14 @@ var gulp = require('gulp');
 var lazypipe = require('lazypipe');
 var less = require('gulp-less');
 var minifyHtml = require('gulp-minify-html');
+var newer = require('gulp-newer');
 var nunjucksRender = require('gulp-nunjucks-render');
 var prettify = require('gulp-prettify');
+var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
+var sourcemaps = require('gulp-sourcemaps');
 //var watch = require('gulp-watch');
-var webserver = require('gulp-webserver');
+//var webserver = require('gulp-webserver');
 
 // @todo properly configure amd optimization
 var amdOptimize = require('gulp-amd-optimizer');
@@ -25,8 +31,14 @@ var paths = {
 	srcComponents: 'src/components/',
 	srcVendor: 'src/vendor/',
 	srcViews: 'src/views/',
-	dist: 'dist/'
+	dist: 'dist/',
+	distAssets: 'dist/assets/'
 };
+paths.assets = [
+	paths.src + 'assets/**/*.*',
+	paths.srcComponents + '*/assets/**/*.*',
+	paths.srcViews + '*/assets/**/*.*'
+];
 // source files are all files directly in module sub directories and core files,
 // excluding abstract files/dirs starting with '_'.
 paths.srcFiles = [
@@ -37,10 +49,13 @@ paths.srcFiles = [
 		'!' + paths.srcComponents + '_*/*',
 		'!' + paths.srcViews + '_*/*'
 ];
+paths.htmlFiles = paths.srcFiles.map(function(path){ return path + '.html'; });
+paths.lessFiles = paths.srcFiles.map(function(path){ return path + '.less'; });
 
 /* Register default & custom tasks (A-Z) */
 gulp.task('default', ['build_clean']);
 gulp.task('build', ['build_html', 'build_less']);
+gulp.task('build_assets', buildAssetsTask);
 gulp.task('build_clean', function(cb) { runSequence('clean_dist', 'build', cb); });
 gulp.task('build_html', buildHtmlTask);
 gulp.task('build_less', buildLessTask);
@@ -50,7 +65,60 @@ gulp.task('jshint_node', jshintNodeTask);
 gulp.task('jshint_src', jshintSrcTask);
 gulp.task('serve', serveTask);
 
+gulp.task('watch', ['build', 'serve'], watchTask);
+
 /* Tasks and utils (A-Z) */
+
+function buildAssetsTask() {
+	paths.assets.map(function(path){
+		return gulp.src(path, { base: paths.src })
+			.pipe(newer(paths.distAssets))
+			.pipe(rename(function(p){
+				p.dirname = p.dirname.replace('/assets', '');
+			}))
+			.pipe(gulp.dest(paths.distAssets));
+	});
+}
+
+function buildHtmlTask() {
+	nunjucksRender.nunjucks.configure([paths.src]);
+	var moduleIndex = getModuleIndex();
+	return srcFiles('html')
+		.pipe(nunjucksRender({
+			moduleIndex: moduleIndex,
+			paths: {
+				assets: '../../assets/'
+			},
+			pkg: pkg
+		}))
+		.pipe(formatHtml())
+		.pipe(gulp.dest(paths.dist))
+		.pipe(reloadBrowser({ stream:true }));
+}
+
+function buildLessTask() {
+	// @todo add sourcemaps
+	return srcFiles('less')
+//		.pipe(sourcemaps.init())
+		.pipe(less())
+//		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.dist))
+		.pipe(reloadBrowser({ stream:true }));
+}
+
+// https://github.com/mariusGundersen/gulp-amd-optimize#sourcemap
+// alternatives:
+// * https://github.com/smrtlabs/smrt-gulp-r
+// * regular rjs + almond with task like clean_dist?
+gulp.task('build_js', function () {
+	var amdConfig = require('./src/amd-config.json');
+	return gulp.src('src/*.js', { base: amdConfig.baseUrl })
+		.pipe(sourcemap.init())
+		.pipe(amdOptimize(amdConfig))
+		.pipe(concat('index.js'))
+		.pipe(sourcemap.write('./', { includeContent: false, sourceRoot: '../src' }))
+		.pipe(gulp.dest(paths.dist));
+});
 
 var formatHtml = lazypipe()
 	.pipe(function() {
@@ -66,34 +134,6 @@ var formatHtml = lazypipe()
 			indent_size: 2
 		});
 	});
-
-function buildHtmlTask() {
-	nunjucksRender.nunjucks.configure([paths.src]);
-	var moduleIndex = getModuleIndex();
-	return srcFiles('html')
-		.pipe(nunjucksRender({
-			moduleIndex: moduleIndex,
-			pkg: pkg
-		}))
-		.pipe(formatHtml())
-		.pipe(gulp.dest(paths.dist));
-}
-
-function buildLessTask() {
-	return srcFiles('less')
-		.pipe(less())
-		.pipe(gulp.dest(paths.dist));
-}
-
-function listModuleDirectories(cwd) {
-	return fs.readdirSync(cwd)
-		.filter(function(file){
-			return fs.statSync(cwd + file).isDirectory();
-		})
-		.filter(function(file){
-			return (file.substr(0,1) !== '_');
-		});
-}
 
 function getModuleIndex() {
 	return {
@@ -114,28 +154,42 @@ function jshintSrcTask() {
 		.pipe(jshint.reporter(require('jshint-stylish')));
 }
 
-function serveTask() {
-	gulp.src(paths.dist)
-		.pipe(webserver({
-			open: true
-		}));
+function listModuleDirectories(cwd) {
+	return fs.readdirSync(cwd)
+		.filter(function(file){
+			return fs.statSync(cwd + file).isDirectory();
+		})
+		.filter(function(file){
+			return (file.substr(0,1) !== '_');
+		});
 }
+
+function reloadBrowser(options){
+	// only reload browserSync if active, otherwise causes an error.
+	return gulpif(browserSync.active, browserSync.reload(options));
+}
+
+function serveTask() {
+	// http://www.browsersync.io/docs/gulp/
+	browserSync({
+		server: {
+			baseDir: paths.dist
+		}
+	});
+}
+//function serveTask() {
+//	gulp.src(paths.dist)
+//		.pipe(webserver({
+//			open: true
+//		}));
+//}
 
 function srcFiles(filetype) {
 	return gulp.src(paths.srcFiles, { base: paths.src })
 		.pipe(filter('**/*.' + filetype));
 }
 
-// https://github.com/mariusGundersen/gulp-amd-optimize#sourcemap
-// alternatives:
-// * https://github.com/smrtlabs/smrt-gulp-r
-// * regular rjs + almond with task like clean_dist?
-gulp.task('build_js', function () {
-	var amdConfig = require('./src/amd-config.json');
-	return gulp.src('src/*.js', { base: amdConfig.baseUrl })
-		.pipe(sourcemap.init())
-		.pipe(amdOptimize(amdConfig))
-		.pipe(concat('index.js'))
-		.pipe(sourcemap.write('./', { includeContent: false, sourceRoot: '../src' }))
-		.pipe(gulp.dest(paths.dist));
-});
+function watchTask () {
+	gulp.watch(paths.htmlFiles, ['build_html']);
+	gulp.watch(paths.lessFiles, ['build_less']);
+}
