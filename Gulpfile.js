0b06c8565f17b08c03c2a4b294be11b4db91ea1c
Jasper Moelker <j.b.moelker@activeids.nl>
2014-10-11 14:11:51 +0200

    chore(setup): add basic templating for views (incl base); gitignore dist

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,13 +2,18 @@
 var del = require('del');
 var filter = require('gulp-filter');
 var jshint = require('gulp-jshint');
+var fs = require('fs');
 var gulp = require('gulp');
+var lazypipe = require('lazypipe');
 var less = require('gulp-less');
 var minifyHtml = require('gulp-minify-html');
 var nunjucksRender = require('gulp-nunjucks-render');
 var prettify = require('gulp-prettify');
+var runSequence = require('run-sequence');
+//var watch = require('gulp-watch');
 var webserver = require('gulp-webserver');
 
+// @todo properly configure amd optimization
 var amdOptimize = require('gulp-amd-optimizer');
 var concat = require('gulp-concat');
 var sourcemap = require('gulp-sourcemaps');
@@ -19,50 +24,82 @@ var paths = {
 	src: 'src/',
 	srcComponents: 'src/components/',
 	srcVendor: 'src/vendor/',
-	srcViews: 'src/views',
+	srcViews: 'src/views/',
 	dist: 'dist/'
 };
-paths.srcFiles = [paths.src + '*', paths.srcComponents + '*/*', paths.srcViews + '*/*'];
+// source files are all files directly in module sub directories and core files,
+// excluding abstract files/dirs starting with '_'.
+paths.srcFiles = [
+		paths.src + '*',
+		paths.srcComponents + '*/*',
+		paths.srcViews + '*/*',
+		'!' + paths.src + '_*',
+		'!' + paths.srcComponents + '_*/*',
+		'!' + paths.srcViews + '_*/*'
+];
 
 /* Register default & custom tasks (A-Z) */
-gulp.task('default', ['build']);
+gulp.task('default', ['build_clean']);
 gulp.task('build', ['build_html', 'build_less']);
+gulp.task('build_clean', function(cb) { runSequence('clean_dist', 'build', cb); });
 gulp.task('build_html', buildHtmlTask);
 gulp.task('build_less', buildLessTask);
-gulp.task('clean_dist', cleanDistTask);
+gulp.task('clean_dist', function (cb) { del([paths.dist], cb); });
 gulp.task('jshint', ['jshint_src', 'jshint_node']);
 gulp.task('jshint_node', jshintNodeTask);
 gulp.task('jshint_src', jshintSrcTask);
 gulp.task('serve', serveTask);
 
 /* Tasks and utils (A-Z) */
-function buildHtmlTask() {
-	nunjucksRender.nunjucks.configure([paths.src]);
-	return gulp.src(paths.srcFiles)
-		.pipe(filter('**/*.html'))
-		.pipe(nunjucksRender({
-			pkg: pkg
-		}))
-		.pipe(minifyHtml({
+
+var formatHtml = lazypipe()
+	.pipe(function() {
+		// strip CDATA, comments & whitespace
+		return minifyHtml({
 			empty: true,
 			conditionals: true,
 			spare: true
-		}))
-		.pipe(prettify({
+		});
+	})
+	.pipe(function() {
+		return prettify({
 			indent_size: 2
+		});
+	});
+
+function buildHtmlTask() {
+	nunjucksRender.nunjucks.configure([paths.src]);
+	var moduleIndex = getModuleIndex();
+	return srcFiles('html')
+		.pipe(nunjucksRender({
+			moduleIndex: moduleIndex,
+			pkg: pkg
 		}))
+		.pipe(formatHtml())
 		.pipe(gulp.dest(paths.dist));
 }
 
 function buildLessTask() {
-	return gulp.src(paths.srcFiles)
-		.pipe(filter('**/*.less'))
+	return srcFiles('less')
 		.pipe(less())
 		.pipe(gulp.dest(paths.dist));
 }
 
-function cleanDistTask (cb) {
-	del([paths.dist], cb);
+function listModuleDirectories(cwd) {
+	return fs.readdirSync(cwd)
+		.filter(function(file){
+			return fs.statSync(cwd + file).isDirectory();
+		})
+		.filter(function(file){
+			return (file.substr(0,1) !== '_');
+		});
+}
+
+function getModuleIndex() {
+	return {
+		components: listModuleDirectories(paths.srcComponents),
+		views: listModuleDirectories(paths.srcViews)
+	};
 }
 
 function jshintNodeTask() {
@@ -72,7 +109,7 @@ function jshintNodeTask() {
 }
 
 function jshintSrcTask() {
-	return gulp.src([paths.src + '**/*.js', '!'+ paths.srcVendor])
+	return srcFiles('js')
 		.pipe(jshint(paths.src + '.jshintrc'))
 		.pipe(jshint.reporter(require('jshint-stylish')));
 }
@@ -84,6 +121,15 @@ function serveTask() {
 		}));
 }
 
+function srcFiles(filetype) {
+	return gulp.src(paths.srcFiles, { base: paths.src })
+		.pipe(filter('**/*.' + filetype));
+}
+
+// https://github.com/mariusGundersen/gulp-amd-optimize#sourcemap
+// alternatives:
+// * https://github.com/smrtlabs/smrt-gulp-r
+// * regular rjs + almond with task like clean_dist?
 gulp.task('build_js', function () {
 	var amdConfig = require('./src/amd-config.json');
 	return gulp.src('src/*.js', { base: amdConfig.baseUrl })
