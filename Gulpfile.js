1594cfd538fb8e5e75f7a27c32ba5305431907eb
Jasper Moelker <j.b.moelker@activeids.nl>
2014-10-30 10:56:42 +0100

    chore(setup): moved config to external file; add markdown support to nunjucks templates

--- Gulpfile.js
+++ Gulpfile.js
@@ -15,6 +15,7 @@ var lazypipe = require('lazypipe');
 var less = require('gulp-less');
 var minifyHtml = require('gulp-minify-html');
 var newer = require('gulp-newer');
+var nunjucksMarkdown = require('nunjucks-markdown');
 var nunjucksRender = require('./lib/nunjucks-render');
 var path = require('path');
 var prettify = require('gulp-prettify');
@@ -26,34 +27,8 @@ var runSequence = require('run-sequence');
 var sourcemaps = require('gulp-sourcemaps');
 
 /* Shared configuration (A-Z) */
+var paths = require('./config.js').paths;
 var pkg = require('./package.json');
-var paths = {
-	src: 'src/',
-	srcComponents: 'src/components/',
-	srcVendor: 'src/vendor/',
-	srcViews: 'src/views/',
-	dist: 'dist/',
-	distAssets: 'dist/assets/',
-	amdConfig: './src/amd-config.json'
-};
-paths.assets = [
-	paths.src + 'assets/**/*.*',
-	paths.srcComponents + '*/assets/**/*.*',
-	paths.srcViews + '*/assets/**/*.*'
-];
-// source files are all files directly in module sub directories and core files,
-// excluding abstract files/dirs starting with '_'.
-paths.srcFiles = [
-		paths.src + '*',
-		paths.srcComponents + '*/*',
-		paths.srcViews + '*/*' //,
-//		'!' + paths.src + '_*',
-//		'!' + paths.srcComponents + '_*/*',
-//		'!' + paths.srcViews + '_*/*'
-];
-paths.htmlFiles = paths.srcFiles.map(function(path){ return path + '.html'; });
-paths.jsFiles   = paths.srcFiles.map(function(path){ return path + '.js'; });
-paths.lessFiles = paths.srcFiles.map(function(path){ return path + '.less'; });
 
 /* Register default & custom tasks (A-Z) */
 gulp.task('default', ['build_clean']);
@@ -151,6 +126,7 @@ function buildLessTask() {
 
 function configureNunjucks() {
 	var env = nunjucksRender.nunjucks.configure(paths.src);
+	nunjucksMarkdown.register(env);
 	env.addFilter('match', require('./lib/nunjucks-filter-match'));
 	env.addFilter('prettyJson', require('./lib/nunjucks-filter-pretty-json'));
 }
